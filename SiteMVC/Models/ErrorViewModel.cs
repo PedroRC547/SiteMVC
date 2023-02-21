1476be7abc71f27e6fcba7a220d774288f731247
Pedro Rodrigues Candiani <53916509+PedroRC547@users.noreply.github.com>
2023-02-21 13:11:19 -0300

    Delete SiteMVC directory

--- SiteMVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SiteMVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
